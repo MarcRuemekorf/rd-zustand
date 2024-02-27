b438683e853f2e22915974dcf38ebaa36b5f8ffa
Marc Ruemekorf <marcruemekorf@users.noreply.github.com>
2024-02-27 19:18:21 +0100

    Add persist method to zustand

--- app/stores/zustand.js
+++ app/stores/zustand.js
@@ -1,10 +1,20 @@
 import { create } from "zustand";
+import { persist, createJSONStorage } from 'zustand/middleware'
 
-export const useStore = create((set) => ({
-  teamA: 0,
-  teamB: 0,
-  increaseTeamAScore: () => set((state) => ({ teamA: state.teamA + 1 })),
-  decreaseTeamAScore: () => set((state) => ({ teamA: state.teamA - 1 })),
-  increaseTeamBScore: () => set((state) => ({ teamB: state.teamB + 1 })),
-  decreaseTeamBScore: () => set((state) => ({ teamB: state.teamB - 1 })),
-}));
+
+export const useStore = create(
+  persist(
+    (set) => ({
+      teamA: 0,
+      teamB: 0,
+      increaseTeamAScore: () => set((state) => ({ teamA: state.teamA + 1 })),
+      decreaseTeamAScore: () => set((state) => ({ teamA: state.teamA - 1 })),
+      increaseTeamBScore: () => set((state) => ({ teamB: state.teamB + 1 })),
+      decreaseTeamBScore: () => set((state) => ({ teamB: state.teamB - 1 })),
+    }),
+    {
+      name: 'zustand-storage',
+      storage: createJSONStorage(() => sessionStorage), // by default, 'localStorage' is used
+    },
+  )
+)
